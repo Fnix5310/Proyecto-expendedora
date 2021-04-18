08d52fd337481ebe7e37a39aba21b26bb7b3b85c
crist <crist@192.168.1.10>
2021-04-18 09:13:32 -0600

    version 1

---
+++ src/expendedora/efecto/ExpendedoraEfecto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expendedora.efecto;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ExpendedoraEfecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        principal v=new principal();
+        v.setVisible(true);
+     //   Efecto.main(args);
+    }
+    
+}
